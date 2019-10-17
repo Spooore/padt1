328ee556cf54b1ebfe8f6f2ca51884e1303ff8b2
Tymon Czarnota <tymon.czarnota@lingarogroup.com>
2019-10-17 23:19:33 +0200

    improvments

--- exclude_vectors.py
+++ exclude_vectors.py
@@ -185,7 +185,6 @@ def main_procesing_corpus(korpus: str, size : int):
     from nltk.corpus import stopwords
     import re
     from glove import Corpus, Glove
-    import numpy
     # glove_6b = r"C:\Users\tymon.czarnota\Desktop\PADT1\glove.6B\glove.6B.100d.txt"
     # word2vec_output_file = r'C:\Users\tymon.czarnota\Desktop\PADT1\glove.6B\glove.6B.100d.txt.word2vec'
     # glove2word2vec(glove_6b, word2vec_output_file)
@@ -212,7 +211,7 @@ def main_procesing_corpus(korpus: str, size : int):
         except FileNotFoundError:
             continue
     
-    print(people_vect_dict)
+    # print(people_vect_dict)
     # with open(glove_6b, "rb") as lines:
     #  wvec = {
     #     line.split()[0].decode('utf-8'): np.array(line.split()[1:], 
@@ -227,13 +226,51 @@ def main_procesing_corpus(korpus: str, size : int):
     glove.save('glove.model')
 
     
+    person_result_dict={}
+    f=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}.tsv".format(korpus,size),'w', encoding ='utf-8')
+    ff=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
     for key in people_vect_dict:
-        print("@@@@" + str(key) + "@@@@")
+        ppl = str(key)
+        for prof in people_dict:
+            for mm in people_dict[prof]:
+                 if str(mm) == str(key):
+                     ppl = ppl + "<--->" + prof + "\n"
         for l in people_vect_dict[key]:
-            for w in l:
-                print(w)
-                print (glove.word_vectors[glove.dictionary[w]])
-    
+            ff.write(ppl)
+            a=[glove.word_vectors[glove.dictionary[w]] for w in l]
+            a_mean=np.mean(a, axis=0, dtype=np.float64)
+            if key not in person_result_dict:
+                person_result_dict[key] = []
+            person_result_dict[key].append(a_mean) 
+            text = ""
+            for val in person_result_dict[key]:
+                for single in val:
+                    text = text + str(single) + "\t" 
+            text = text + "\n" 
+            f.write(text)
+            
+    f=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_WHOLE.tsv".format(korpus,size),'w', encoding ='utf-8')
+    ff=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_WHOLE_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
+    for key in person_result_dict:
+        a=np.mean(person_result_dict[key], axis=0, dtype=np.float64)
+        str_a = ""
+        for el in a:
+            str_a = str_a + str(el) + "\t"
+        str_a = str_a + "\n"
+        f.write(str_a)
+        
+        str_key = ""
+        for prof in people_dict:
+            for mm in people_dict[prof]:
+                 if str(mm) == str(key):
+                     str_key = str(key) + "<--->" + prof + "\n"
+        ff.write(str_key)
+        
 main_procesing_corpus("korpusGAZETA",3)
 main_procesing_corpus("korpusGAZETA",4)
 main_procesing_corpus("korpusGAZETA",5)
+
+
+main_procesing_corpus("korpusONET",3)
+main_procesing_corpus("korpusONET",4)
+main_procesing_corpus("korpusONET",5)
