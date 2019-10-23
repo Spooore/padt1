f20b63145d678c582e66891254aad255a3056bbe
Tymon Czarnota <tymon.czarnota@lingarogroup.com>
2019-10-23 21:19:53 +0200

    korpus

--- exclude_vectors.py
+++ exclude_vectors.py
@@ -191,7 +191,7 @@ def main_procesing_corpus(korpus: str, size : int):
 
     
     
-    path_raw = r"C:\Users\tymon.czarnota\Desktop\PADT1\RawData\{}".format(korpus)
+    path_raw = r"C:\Users\tymon.czarnota\Desktop\Pulpit\PADT1\RawData\{}".format(korpus)
     file_number = max([ int(re.findall(r'\d+', name)[0]) for name in os.listdir(path_raw)])
     people_vect_dict= {}
     my_corpus = []
@@ -221,14 +221,16 @@ def main_procesing_corpus(korpus: str, size : int):
     corpus.fit(my_corpus, window=10)
     
     glove = Glove(no_components=100, learning_rate=0.05)
-    glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
+    print("Fit Started")
+    glove.fit(corpus.matrix, epochs=400, no_threads=4, verbose=False)
+    print("Fit Finished")
     glove.add_dictionary(corpus.dictionary)
     glove.save('glove.model')
 
     
     person_result_dict={}
-    f=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}.tsv".format(korpus,size),'w', encoding ='utf-8')
-    ff=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
+    f=open(r"C:\Users\tymon.czarnota\Desktop\Pulpit\PADT1\output_{}_{}.tsv".format(korpus,size),'w', encoding ='utf-8')
+    ff=open(r"C:\Users\tymon.czarnota\Desktop\Pulpit\PADT1\output_{}_{}_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
     for key in people_vect_dict:
         ppl = str(key)
         for prof in people_dict:
@@ -249,10 +251,12 @@ def main_procesing_corpus(korpus: str, size : int):
             text = text + "\n" 
             f.write(text)
             
-    f=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_WHOLE.tsv".format(korpus,size),'w', encoding ='utf-8')
-    ff=open(r"C:\Users\tymon.czarnota\Desktop\PADT1\output_{}_{}_WHOLE_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
+    f=open(r"C:\Users\tymon.czarnota\Desktop\Pulpit\PADT1\output_{}_{}_WHOLE.tsv".format(korpus,size),'w', encoding ='utf-8')
+    ff=open(r"C:\Users\tymon.czarnota\Desktop\Pulpit\PADT1\output_{}_{}_WHOLE_META.tsv".format(korpus,size),'w',encoding = 'utf-8')
     for key in person_result_dict:
-        a=np.mean(person_result_dict[key], axis=0, dtype=np.float64)
+        a=np.mean( person_result_dict[key], axis=0, dtype=np.float64)
+        
+        
         str_a = ""
         for el in a:
             str_a = str_a + str(el) + "\t"
@@ -267,10 +271,10 @@ def main_procesing_corpus(korpus: str, size : int):
         ff.write(str_key)
         
 main_procesing_corpus("korpusGAZETA",3)
-main_procesing_corpus("korpusGAZETA",4)
-main_procesing_corpus("korpusGAZETA",5)
+# main_procesing_corpus("korpusGAZETA",4)
+# main_procesing_corpus("korpusGAZETA",5)
 
 
-main_procesing_corpus("korpusONET",3)
-main_procesing_corpus("korpusONET",4)
-main_procesing_corpus("korpusONET",5)
+# main_procesing_corpus("korpusONET",3)
+# main_procesing_corpus("korpusONET",4)
+# main_procesing_corpus("korpusONET",5)
